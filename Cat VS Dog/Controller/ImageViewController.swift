1c4b86a3740861d478e36877c4c95eff3760a68a
garima94921 <gaarimabothra@gmail.com>
2020-05-06 20:44:03 +0530

    Relocated files

--- Cat VS Dog/Controller/ImageViewController.swift
+++ Cat VS Dog/Controller/ImageViewController.swift
@@ -60,33 +60,3 @@ class ImageViewController: UIViewController {
         }
     }
 }
-
-extension UIImage {
-
-    func pixelBuffer() -> CVPixelBuffer? {
-// NOTE: Create pixel buffer with specified pixel format
-var pixelBuffer: CVPixelBuffer?
-let options = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
-let status = CVPixelBufferCreate(kCFAllocatorDefault, 64, 64, kCVPixelFormatType_32ARGB, options, &pixelBuffer)
-guard status == kCVReturnSuccess, let finalPixelBuffer = pixelBuffer else {
-    return nil
-}
-        // NOTE: Create context ("canvas") for drawing pixels
-               CVPixelBufferLockBaseAddress(finalPixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
-               let pixelData = CVPixelBufferGetBaseAddress(finalPixelBuffer)
-               let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
-               guard let context = CGContext(data: pixelData, width: Int(64), height: 64, bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(finalPixelBuffer), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) else {
-                   return nil
-               }
-               context.translateBy(x: 0, y: 64)
-               context.scaleBy(x: 1.0, y: -1.0)
-
-               // NOTE: Draw pixels on the context, updates pixel buffer
-               UIGraphicsPushContext(context)
-               draw(in: CGRect(x: 0, y: 0, width: 64, height: 64))
-               UIGraphicsPopContext()
-               CVPixelBufferUnlockBaseAddress(finalPixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
-
-               return finalPixelBuffer
-}
-}
---
+++ Cat VS Dog/UIImageExtension.swift
@@ -0,0 +1,40 @@
+//
+//  UIImageExtension.swift
+//  Cat VS Dog
+//
+//  Created by Garima Bothra on 06/05/20.
+//  Copyright © 2020 Garima Bothra. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+
+    func pixelBuffer() -> CVPixelBuffer? {
+// NOTE: Create pixel buffer with specified pixel format
+var pixelBuffer: CVPixelBuffer?
+let options = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+let status = CVPixelBufferCreate(kCFAllocatorDefault, 64, 64, kCVPixelFormatType_32ARGB, options, &pixelBuffer)
+guard status == kCVReturnSuccess, let finalPixelBuffer = pixelBuffer else {
+    return nil
+}
+        // NOTE: Create context ("canvas") for drawing pixels
+               CVPixelBufferLockBaseAddress(finalPixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+               let pixelData = CVPixelBufferGetBaseAddress(finalPixelBuffer)
+               let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+               guard let context = CGContext(data: pixelData, width: Int(64), height: 64, bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(finalPixelBuffer), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) else {
+                   return nil
+               }
+               context.translateBy(x: 0, y: 64)
+               context.scaleBy(x: 1.0, y: -1.0)
+
+               // NOTE: Draw pixels on the context, updates pixel buffer
+               UIGraphicsPushContext(context)
+               draw(in: CGRect(x: 0, y: 0, width: 64, height: 64))
+               UIGraphicsPopContext()
+               CVPixelBufferUnlockBaseAddress(finalPixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+
+               return finalPixelBuffer
+}
+}
